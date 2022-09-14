a3c57b3eff0b42a7d6d84d8e533f5c2cfd2e8421
Revanthraja M <revanthrajam@gmail.com>
2022-09-14 22:09:26 +0530

    Update Silk.py

--- Silk.py
+++ Silk.py
@@ -105,4 +105,4 @@ model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 
 x=model.fit(x_train,y_train,batch_size=10,verbose=1,epochs=15,validation_data=(x_test,y_test),shuffle=False)
-model.save('Silkworm10Epoch.h6')
+model.save('Silkworm10Epoch.h5')
